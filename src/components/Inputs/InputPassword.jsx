6a7c97b9dd6acb1c530cc85faa31cbf79f190a56
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-03-08 10:57:15 +0100

    create component Input type password

---
+++ src/components/Inputs/InputPassword.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+function InputPassword() {
+
+    const [isFocused, setIsFocused] = useState(false);
+
+    const handleFocus = () => {
+        setIsFocused(true);
+    }
+
+    const handleBlur = () => {
+        setIsFocused(false);
+    }
+
+  return (
+    <div className='relative'>
+        <input
+            type='password'
+            className='font-bold bg-[#290D59] text-white rounded-lg pl-4 p-1 pt-4'
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            required
+        />
+        <label
+            htmlFor='inputPassword'
+            className={`absolute left-4 ${
+                isFocused ? '-top-2 text-[#F9E900] text-sm pt-2' : 'mt-2 text-[#F9E900]'
+              } pointer-events-none transition-all duration-300`}
+        >
+            Password
+        </label>
+    </div>
+  )
+}
+
+export default InputPassword
