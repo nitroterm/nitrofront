012101614713fdb9f9ec7465241aab26146ba788
geoffrey <geoffrey.coutant@ynov.com>
2024-04-04 13:33:02 +0200

    reduced size button like

--- src/components/Buttons/ButtonLike.jsx
+++ src/components/Buttons/ButtonLike.jsx
@@ -11,13 +11,13 @@ function ButtonLike() {
   return (
     <div className='mx-4'>
       <button 
-        className={`flex items-center justify-center ${ clicked ? 'bg-[#FF5D70]' : 'bg-[#290D59]' } bg-[#290D59] ${ clicked ? 'hover:bg-[#FF5D70]' : 'hover:bg-[#411A83]' } hover:bg-[#411A83] rounded-xl w-14 h-14`}
+        className={`flex items-center justify-center ${ clicked ? 'bg-[#FF5D70]' : 'bg-[#290D59]' } bg-[#290D59] ${ clicked ? 'hover:bg-[#FF5D70]' : 'hover:bg-[#411A83]' } hover:bg-[#411A83] rounded-xl w-10 h-10`}
         onClick={handleClick}
         >
         <img 
           src={require('../img/like.png')} 
           alt='nitro' 
-          className='w-7 h-6' 
+          className='w-5 h-4'
         />
       </button>
     </div>
