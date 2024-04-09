4e48bd74874cc62b90e0aa17ca684303bb76e405
Cacahuète Dev <24205659+CacahueteSansSel@users.noreply.github.com>
2024-04-09 14:00:35 +0200

    fully added recaptcha to register

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ReCAPTCHA from "react-google-recaptcha";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
