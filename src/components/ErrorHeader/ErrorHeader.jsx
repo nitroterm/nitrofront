af80ea285b59e33e14f7430ddff151c369deb6a5
geoffrey <geoffrey.coutant@ynov.com>
2024-04-09 16:31:54 +0200

    add components errorheader, fixe bug and add cross

--- src/components/ErrorHeader/ErrorHeader.jsx
+++ src/components/ErrorHeader/ErrorHeader.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import cross from '../img/cross.svg';
 
-function PasswordError({ showError }) {
+function PasswordError({ showError, errorMessage }) {
     return (
-        <div>{showError && <div className="text-red-500">Password mismatch. Please try again.</div>}</div>
+        <div>
+            {showError && <div className="flex bg-[#590D0D] border border-[#9D2121] text-xs p-2 mb-2 rounded font-['PT Sans]"><img className='h-4 mr-2' src={cross} alt='cross'/>{errorMessage}</div>}
+        </div>
     );
 }
 
--- src/components/WrapperRegister/WrapperRegister.jsx
+++ src/components/WrapperRegister/WrapperRegister.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import InputText from "../Inputs/InputText";
 import InputPassword from "../Inputs/InputPassword";
 import {Link} from "react-router-dom";
 import secureLocalStorage from "react-secure-storage";
 import ReCAPTCHA from "react-google-recaptcha";
-const recaptchaRef = React.createRef();
 import ErrorHeader from "../ErrorHeader/ErrorHeader";
 
+const recaptchaRef = React.createRef();
+
 function WrapperRegister({textLogin, text}) {
+    const [loginSuccess, setLoginSuccess] = useState(true);
+    const [errorMessage, setErrorMessage] = useState("");
     return (
-        <div className='flex bg-[#080016] text-white pb-20 pl-10 border border-purple-900 rounded-lg'>
+        <div className='flex bg-[#080016] text-white pb-10 pl-10 border border-purple-900 rounded-lg'>
             <div className='flex flex-col mb-44 mr-36 mt-8'>
                 <img className='w-64 h-24 pb-4' src={require('../img/logo.png')} alt="logo"/>
                 <h1 className='font-bold pt-6 pl-5 text-3xl text-[#F9E900]'>Create your account</h1>
             </div>
             <div className='flex flex-col items-center mr-16 justify-center'>
-                <div className='flex flex-col gap-2 mt-36'>
+                <div className='flex flex-col gap-2 mt-28'>
+                    <div className='h-10'>
+                        <ErrorHeader showError={!loginSuccess} errorMessage={errorMessage} />
+                    </div>
                     <InputText id="input_username" placeholder='Username'/>
                     <InputPassword id="input_password" placeholder='Password'/>
                     <InputPassword id="input_password_confirm" placeholder='Confirm Password'/>
@@ -28,7 +34,7 @@ function WrapperRegister({textLogin, text}) {
                     </form>
                 </div>
                 <div className='flex flex-col mt-12 gap-2'>
-                    <button onClick={sendRegister}
+                    <button onClick={() => sendRegister(setLoginSuccess, setErrorMessage)}
                             className='text-black bg-[#F9E900] hover:bg-[#FFF564] w-96 p-2 transition duration-300 rounded-lg border border-yellow-300 px-10 font-bold text-[15px]'>{textLogin}</button>
                     <Link to="/login"><button className='text-white bg-[#290D59] hover:bg-[#411A83] w-96 p-2 transition duration-300 rounded-lg border border-purple-900 px-10 font-bold text-[15px]'>{text}</button></Link>
                 </div>
@@ -37,12 +43,15 @@ function WrapperRegister({textLogin, text}) {
     );
 }
 
-function sendRegister() {
+function sendRegister(setLoginSuccess, setErrorMessage) {
+    const challenge = recaptchaRef.current.getValue();
+
     if (document.getElementById('input_password').value !== document.getElementById('input_password_confirm').value) {
-        alert('Passwords do not match');
+        setErrorMessage("Username or password is invalid");
+        setLoginSuccess(false);
         return;
     }
-    const challenge = recaptchaRef.current.getValue();
+
     fetch('https://services.cacahuete.dev/api/nitroterm/v1/auth/register', {
         method: 'POST',
         headers: {
@@ -57,7 +66,7 @@ function sendRegister() {
         .then((response) => response.json())
         .then((data) => {
             if (!data.success) {
-                alert(data.message);
+                setErrorMessage(data.message);
                 return;
             }
 
@@ -66,7 +75,7 @@ function sendRegister() {
             window.location.href = '/#onboarding';
         })
         .catch((error) => {
-            console.error('Error:', error.message);
+            setErrorMessage(error.message);
         });
 }
 
