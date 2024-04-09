8f42b1ec5347a4b79b4132d36de4e5abb67efd3c
FlorentinFallon <119119355+FlorentinFallon@users.noreply.github.com>
2024-04-09 16:17:38 +0200

    modified Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 function Sidebar() {
   return (
-    <div className='ml-16 mt-12 flex flex-col gap-[30rem]'>
+    <div className='ml-16 mt-12 flex flex-col gap-[30rem] md:gap-96'>
         <div className='mt-12'>
             <Link to="/createpost"><ButtonRegister text="Write something"/></Link>
             <ButtonMenu text="Your Feed"/>
